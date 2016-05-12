7e6c59c2f73763d9d89f580352b1f2cf2106282a
CristaudoORG <cristaudo00@gmail.com>
2016-05-12 16:56:47 +0200

    Update Main.php

--- src/ImagicalGamer/PlayerHead/Main.php
+++ src/ImagicalGamer/PlayerHead/Main.php
@@ -30,19 +30,46 @@ use pocketmine\nbt\tag\IntTag as I;
 use pocketmine\nbt\tag\ListTag as Enum;
 use pocketmine\tile\Tile;
 use pocketmine\level\format\FullChunk;
+use pocketmine\utils\config;
 
 class Main extends PluginBase implements Listener{
   public function onEnable(){
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
     $this->getLogger()->info(C::GREEN . "Enabled!");
+    $this->saveResource("/config.yml");
+    
+    @mkdir($this->getDataFolder());
+    $config = new Config($this->getDataFolder() . "world.yml", Config::YAML);
+    $config->save()
   }
   public function onDeath(PlayerDeathEvent $event){
+    $rand = rand(1, 5);
+    switch($rand){
+    	case '1':
+    		$head = 397;
+    	break;
+        case '2':
+        	$head = 397:1;
+        break;
+        case '3':
+        	$head = 397:2;
+        break;
+        case '4':
+        	$head = 397:3;
+        break;
+        case '5':
+        	$head = 397:4;
+        break;
+    }
     $item = Item::get(397, 3, 1);
     $e = $event->getEntity();
     $level = $e->getLevel();
     $x = $e->getX();
     $y = $e->getY();
     $z = $e->getZ();
+    $sth = $config->getAll();
+    $worlds = $sth["Worlds"];
+    if($level->getName()!= $worlds){
     if($e instanceof Player){
 	$level->setBlock(new Vector3($x,$y,$z), Block::get(144));
       $name = $e->getName();
@@ -54,6 +81,10 @@ class Main extends PluginBase implements Listener{
            $killer->getInventory()->addItem($item);
            $item->setCustomName($name . "'s head");
          }
+    }
+    }
+    else{
+    	$event->setCancelled(true);
     }
   }
 }
